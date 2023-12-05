b629c8de66009bf4d6d798fa03b975c4ee2228ab
Ibelema <ibelemaiwoh02@gmail.com>
2023-12-05 23:15:54 +0100

    the Product was updated

--- apps/general/views.py
+++ apps/general/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-# Create your views here.
-class AboutView(view):
-    def get(self, request):
-        return render(request, "general/about.html")
 
-class ContactView(view):
+# Create your views here.
+
+
+class AboutView(View):
+    def get(self, request):
+        sitedetail, created = SiteDetail.objects.get_or_create()
+        teammembers = TeamMember.objects.all()
+        context = {"sitedetail": sitedetail, "teammembers": teammembers}
+        return render(request, "general/about.html", context)
+
+
+class ContactView(View):
     def get(self, request):
         return render(request, "general/contact.html")
-        
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from apps.accounts.models import User
 from apps.shop.models import Category, Product
-
+from django.views.generic import ListView
 # Create your views here.
 
 
@@ -16,3 +16,11 @@ class HomeView(View):
             "rating_range": range(5),
         }
         return render(request, "shop/home.html", context)
+
+
+class ProductsView(ListView):
+    model = Product
+    paginate_by = 15
+    template_name = "shop/products.html"
+    context_object_name = "products" 
+    queryset = Product.objects.prefetch_related("reviews")
